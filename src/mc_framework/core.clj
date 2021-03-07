e8d2449c8bacb58ffd3c5f86a1339f221e55afad
Ilja Faerman <ifaerman@numerix.com>
2021-03-07 14:13:55 +0100

    eq-option protocol

--- src/mc_framework/core.clj
+++ src/mc_framework/core.clj
@@ -5,3 +5,37 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+
+(defprotocol Payoff
+  (payoff-function [product])
+  (generate-payoff [product spot])
+  (generate-payoff-pathwise [product paths]))
+
+
+(defrecord Eq-european-call [underlying strike expiry]
+  Payoff
+  (payoff-function [product] (map #(max 0 (- % strike))))
+  (generate-payoff [product spot] (max 0 (- spot strike)))
+  (generate-payoff-pathwise [product paths] (transduce (payoff-function product) conj paths)))
+
+(defrecord Eq-barrier-call [underlying strike expiry barrier]
+  Payoff
+  (generate-payoff [product spot] (if (< spot barrier) (max 0 (- spot strike) 0) 0)))
+
+(def eq-european-1 (->Eq-european-call :aapl 100 0.5))
+(def eq-barrier-1 (->Eq-barrier-call :aapl 90 0.5 110))
+
+(def paths [100 150 230 95 20])
+(generate-payoff-pathwise eq-european-1 paths)
+
+(generate-payoff eq-european-1 100)
+(generate-payoff eq-barrier-1 100)
+
+(generate-payoff (->Eq-barrier-call :aapl 90 0.5 99) 93)
+
+(comment 
+  (init-simulation)
+  (generate-paths)
+  (compute-payoff))
+
